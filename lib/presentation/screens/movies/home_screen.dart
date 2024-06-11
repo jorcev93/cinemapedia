433807a4bcf5c3d6875c213a4ac77c1f6e16631c
jorcev93 <jorge.ceva93@gmail.com>
2024-06-11 13:00:15 -0500

    Se creo el appbar, carrusel de peliculas y SwiperPagination

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart'
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
   static const name =
       'home-screen'; //nombre de la ruta que va autilizar go_router para esta pantalla
@@ -47,16 +49,32 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     //aqui vamos a renderizar la data, es decir vamos a poder ver la data en la pantalla
-    final nowPlayingMovies = ref
-        .watch(nowPlayingMoviesProvider); //este va a ser el listado de peliclas
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider); //este va a ser el listado de peliclas
+    return Column(
+      children: [
+        const CustomAppbar(),
+        MoviesSlideshow(movies: nowPlayingMovies),
+      ],
     );
+    /*Este expanden ya lo puedo comentar solo lo utilice para saber que si se mostraban las peliculas */
+    /*return Column(
+      children: [
+        const CustomAppbar(),//mando a llamar el widget que cree en la carpeta widgets
+        //cuando utilizo colum tambien tengo que utilizar Expanded, para que no se vaya hasta el infinito
+        //el expanden hace que dado el padre(Clolumn), ocupe tiodo el espacio posible, 
+        //por que este ya tiene un alto y ancho fijo, cabe mencionar que hay algunos widgets que hacen algo similar
+        Expanded(
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context, index) {
+              final movie = nowPlayingMovies[index];
+              return ListTile(
+                title: Text(movie.title),
+              );
+            },
+          ),
+        )
+      ],
+    );*/
   }
 }
---
+++ lib/presentation/screens/widgets/widgets.dart
@@ -0,0 +1,3 @@
+//este es mi archivo barril para los widgets
+export 'package:cinemapedia/presentation/screens/widgets/movies_slideshow.dart';
+export 'package:cinemapedia/presentation/screens/widgets/shared/custom_appbar.dart';
