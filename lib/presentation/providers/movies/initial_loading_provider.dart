a41c9e7270238451770b98ac5f6d05ceb85e28f6
jorcev93 <jorge.ceva93@gmail.com>
2024-06-14 09:35:56 -0500

    fin

--- lib/presentation/providers/movies/initial_loading_provider.dart
+++ lib/presentation/providers/movies/initial_loading_provider.dart
@@ -1,9 +1,6 @@
 //este provider es de solo lectura
 //este provider va a servor para determinar si presentar el full_screen_loader
 //o el "CustomScrollView", que se encuentra en el home_screen
-
-import 'package:flutter/material.dart';
-
 import 'movies_providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,6 +9,6 @@ final initialLoadingProvider = Provider<bool>((ref) {
   final step2 = ref.watch(popularMoviesProvider).isEmpty;
   final step3 = ref.watch(upcomingMoviesProvider).isEmpty;
   final step4 = ref.watch(topRatedMoviesProvider).isEmpty;
-  if (step1 || step4 || step3 || step4) return true;//
+  if (step1 || step2 || step4 || step3 || step4) return true;//
   return false; //terminamos de cargar
 });
