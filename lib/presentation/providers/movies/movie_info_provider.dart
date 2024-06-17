d2facec5c560e9b1725b4edd62025d5acbcc9aad
jorcev93 <jorge.ceva93@gmail.com>
2024-06-17 16:26:15 -0500

    peticion http y cache local

--- lib/presentation/providers/movies/movie_info_provider.dart
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -4,14 +4,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 //qui hacemos la implementacion de este provider que es de tipo StateNotifierProvider
-final movieInfoProvider = StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref) {
-  final movieRepository = ref.watch( movieRepositoryProvider );
-  return MovieMapNotifier(getMovie: movieRepository.getMovieById );
+final movieInfoProvider =
+    StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref) {
+  final movieRepository = ref.watch(movieRepositoryProvider);
+  return MovieMapNotifier(getMovie: movieRepository.getMovieById);
 });
 
 //asi quiero que funcione el state de este provider
 //es decir que voy a definir un map con el id de la pelicula y va a apuntar una instancia de movie
-//entonces voy a ir preguntando si esque la pelicula ya se consulto y si esq no se hace una nueva peticion 
+//entonces voy a ir preguntando si esque la pelicula ya se consulto y si esq no se hace una nueva peticion
 /*
   {
     '505642': Movie(),
@@ -21,22 +22,21 @@ final movieInfoProvider = StateNotifierProvider<MovieMapNotifier, Map<String, Mo
   }
 */
 
-typedef GetMovieCallback = Future<Movie>Function(String movieId);
+typedef GetMovieCallback = Future<Movie> Function(String movieId);
 
-class MovieMapNotifier extends StateNotifier<Map<String,Movie>> {
-
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
   final GetMovieCallback getMovie;
 
   //constructor de la clase
   MovieMapNotifier({
     required this.getMovie,
-  }): super({});
+  }) : super({});
 
   //qui mandamos a llamar la implementacion que trae la pelicula
-  Future<void> loadMovie( String movieId ) async {
-    if ( state[movieId] != null ) return;//si el state tiene una pelicula con ese id entonces ya no voy a regresar nada 
-    final movie = await getMovie( movieId );//caso contrario pedymos la pelicula
-    state = { ...state, movieId: movie };//actualizamos el estado
-  }
+  Future<void> loadMovie(String movieId) async {
+    if (state[movieId] != null)return; //si el state tiene una pelicula con ese id entonces ya no voy a regresar nadaprint('realizando una peticion http');
 
+    final movie = await getMovie(movieId); //caso contrario pedymos la pelicula
+    state = {...state, movieId: movie}; //actualizamos el estado
+  }
 }
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,31 +1,59 @@
+//NOTA: EL SCOPE, ES TODA LA CLASE DE ESTADO
+
+//NOTA: COMO ESTAMOS UTILIZANDO EL ConsumerState el ref PODEMOS UTILIZARLO DIRECTAMENTE EN EL initState
+//POR EL CONTRARIO SI LO ESTUVIÉRAMOS UTILIZANDO EN EL StatefulWidget, LO UTILIZRIOAMOS EN EL build
+
+//NOTA: SE UTILIZA ref.read, cuando estamos en metodos que no se pueden redibujar,
+// por ejemplo en callback, ontap, onpress, en el init...
+
 //Esta pantalla va a ser para presentar los datos de una sola pelicula
 
 //el StatefulWidget, me sirve para saber cuando cargo o estoy cargando entre otras cosas, utilizando el inisState
+import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatefulWidget {
+import '../../../domain/entities/movie.dart';
+
+//vamos a cambiar el StatefulWidget por un ConsumerStatefulWidget, para poder teer acceso al scope de de nuestro provider
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'mvie-screen';
   final String movieId;
   const MovieScreen({super.key, required this.movieId});
 
+  //como ya cambiamos el State por un ConsumerState y solo tenemos que llamar al metodo
+  //State<MovieScreen> createState() => _MovieScreenState();
   @override
-  State<MovieScreen> createState() => _MovieScreenState();
+  MovieScreenState createState() => MovieScreenState();
 }
 
-class _MovieScreenState extends State<MovieScreen> {
-  
-  //me sirve para manejar un cache local, para efienizar y no estar haceindo una nueva peticion si esq ya la hicimos anterormente
+//como cambiamos el StatefulWidget por un ConsumerStatefulWidget, tambien debemos cambiar el State, por un ConsumerState
+class MovieScreenState extends ConsumerState<MovieScreen> {
+  //me sirve para manejar un cache local, para efientizar y no estar haceindo una nueva peticion si esq ya la hicimos anterormente
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    //esto realiza la peticion http, para traer los datos de la peli por el id
+    ref.read(movieInfoProvider.notifier).loadMovie(widget
+        .movieId); //se utiliza widget antes de movieID, por qe estamos dentro del initstate
   }
 
   @override
   Widget build(BuildContext context) {
+    //variable para saber si hay la pelicula
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+    //validamos si esq existe la pelicula
+    if (movie == null) {
+      return const Scaffold(
+          body: Center(
+        child: CircularProgressIndicator(
+          strokeWidth: 2,
+        ),
+      ));
+    }
     return Scaffold(
         appBar: AppBar(
-          title: Text('MovieID: ${widget.movieId}'),
+      title: Text('MovieID: ${widget.movieId}'),
     ));
   }
 }
