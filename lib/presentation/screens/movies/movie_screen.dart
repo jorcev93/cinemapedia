1eefd439beeee8f2d3b8f84dae853f1ad7d8e5e7
jorcev93 <jorge.ceva93@gmail.com>
2024-06-18 13:00:29 -0500

    descripcion de la pelicula

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -44,14 +44,19 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     //validamos si es que existe la pelicula
     if (movie == null) {
       return const Scaffold(
-        body: Center(child: CircularProgressIndicator(strokeWidth: 2)));
+          body: Center(child: CircularProgressIndicator(strokeWidth: 2)));
     }
     return Scaffold(
       //voy a tulizar el CustomScrollView, por que voy a emplear los slivers
       body: CustomScrollView(
-        physics: const ClampingScrollPhysics(),//esto es para quitar el efecto rebote que sabe tener en ios
+        physics:
+            const ClampingScrollPhysics(), //esto es para quitar el efecto rebote que sabe tener en ios
         slivers: [
           _CustomSliverAppBar(movie: movie),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  (context, index) => _MovieDetails(movie: movie),
+                  childCount: 1))
         ],
       ),
     );
@@ -64,11 +69,13 @@ class _CustomSliverAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size; //con esto se el tamaño del dipositivo
+    final size =
+        MediaQuery.of(context).size; //con esto se el tamaño del dipositivo
     //aqui vamos a empezar a construir un appbar perzonalizado
     return SliverAppBar(
       backgroundColor: Colors.black,
-      expandedHeight: size.height * 0.7, //aqui estoy diciendo que va a tomar el 70% de la pantalla
+      expandedHeight: size.height *
+          0.7, //aqui estoy diciendo que va a tomar el 70% de la pantalla
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
@@ -80,8 +87,12 @@ class _CustomSliverAppBar extends StatelessWidget {
         //el stack me permite colocar widgets unos sobre ottros
         background: Stack(
           children: [
-            SizedBox.expand(//el expanded es para que tome todo el espacio posible del padre
-              child: Image.network(movie.posterPath, fit: BoxFit.cover,),
+            SizedBox.expand(
+              //el expanded es para que tome todo el espacio posible del padre
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+              ),
             )
           ],
         ),
@@ -90,3 +101,68 @@ class _CustomSliverAppBar extends StatelessWidget {
   }
 }
 
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+  const _MovieDetails({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyles = Theme.of(context).textTheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,//va a estar alineado al inicio
+      children: [
+         Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              
+              // Imagen
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.3,
+                ),
+              ),
+
+              const SizedBox( width: 10 ),
+
+              // Descripción
+              SizedBox(
+                width: (size.width - 40) * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text( movie.title, style: textStyles.titleLarge ),
+                    Text( movie.overview ),
+                  ],
+                ),
+              )
+            ]
+          )
+         ),
+
+          // Generos de la película
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map((gender) => Container(
+                margin: const EdgeInsets.only( right: 10),
+                child: Chip(
+                  label: Text( gender ),
+                  shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20)),
+                ),
+              ))
+            ],
+          ),
+        ),
+        
+         const SizedBox(height: 100,)
+      ],
+
+    );
+  }
+}
