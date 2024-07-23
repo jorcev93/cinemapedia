3bf4c79caf2b773b93224735b09d34238b36c426
jorcev93 <jorge.ceva93@gmail.com>
2024-07-23 10:39:40 -0500

    shell route

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,11 +4,43 @@ import 'package:cinemapedia/presentation/views/home_views/favorites_view.dart';
 import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 import '../../presentation/screens/screens.dart';
+
 //aqui va la configuracion de rutas
 final appRouter = GoRouter(
     initialLocation: '/', //
     routes: [
-      GoRoute(
+      ShellRoute(
+          builder: (context, state, child) {
+            return HomeScreen(childView: child);
+          },
+          routes: [
+            GoRoute(
+                path: '/',
+                builder: (context, state) {
+                  return const HomeView();
+                },
+                routes: [
+                  GoRoute(
+                    //como es una ruta hija, ya no va el primer "/", pasaria de esto "'/movie/:id'" a esto "'movie/:id'"
+                    path:'movie/:id', //aqui estoy diciendo que va a recibir un id, y este siempre va aser un strig
+                    name: MovieScreen.name,
+                    builder: (context, state) {
+                      //esta variable es para obtener el parametro del id que viene en el path
+                      final movieId = state.pathParameters['id'] ?? 'no-id';
+                      return MovieScreen(movieId: movieId);
+                    },
+                  )
+                ]),
+            GoRoute(
+              path: '/',
+              builder: (context, state) {
+                return const FavoritesView();
+              },
+            )
+          ]),
+
+      //Rutas padre/hijo
+      /*GoRoute(
           path: '/',
           name: HomeScreen.name,
           builder: (context, state) => const HomeScreen(childView: HomeView(),),
@@ -23,7 +55,7 @@ final appRouter = GoRouter(
                   final movieId = state.pathParameters['id'] ?? 'no-id';
                   return MovieScreen(movieId: movieId);
                 })
-          ]),
+          ]),*/
 
       //si lo dejo aqui en el momento que se recarga la pagina, se va a perder
       //el boton de regreso, para ello es mejor ubicarlo dentro de la ruta de  "HomeScreen"
