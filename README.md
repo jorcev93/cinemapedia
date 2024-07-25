fff27ef8db3158b875f7d221c724a3a1b605118e
jorcev93 <jorge.ceva93@gmail.com>
2024-07-24 19:40:25 -0500

    isar database

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 # Dev
 1. Copiar el .env.template y renomdrarlo a .env
 2. cambiar las variables de entorno (THE_MOVIEDB_KEY)
-
+3. Cambios en la entidad, hay que ejecutar el comando 
+......
+flutter pub run build_runner build
+......
 A new Flutter project.
--- lib/domain/entities/movie.dart
+++ lib/domain/entities/movie.dart
@@ -1,4 +1,13 @@
+//NOTA AUNA VEZ INSTALADO Isar, Modificamos esta entidad y las que necesitemos
+import 'package:isar/isar.dart';
+
+//cuando se agrega este part no importa si da error, por que es un archivo que se va a crear automaticamente
+//cuando abramos la terminal y ejecutemos el comando  "flutter pub run build_runner build"
+part 'movie.g.dart';
+
+@collection //agregamos este collection una vez que instalamos isar
 class Movie {
+  Id? isarId; //lo hago opcional para que isar autoincremente automaticamente
   final bool adult;
   final String backdropPath;
   final List<String> genreIds;
@@ -14,20 +23,19 @@ class Movie {
   final double voteAverage;
   final int voteCount;
 
-  Movie({
-    required this.adult,
-    required this.backdropPath,
-    required this.genreIds,
-    required this.id,
-    required this.originalLanguage,
-    required this.originalTitle,
-    required this.overview,
-    required this.popularity,
-    required this.posterPath,
-    required this.releaseDate,
-    required this.title,
-    required this.video,
-    required this.voteAverage,
-    required this.voteCount
-  });
+  Movie(
+      {required this.adult,
+      required this.backdropPath,
+      required this.genreIds,
+      required this.id,
+      required this.originalLanguage,
+      required this.originalTitle,
+      required this.overview,
+      required this.popularity,
+      required this.posterPath,
+      required this.releaseDate,
+      required this.title,
+      required this.video,
+      required this.voteAverage,
+      required this.voteCount});
 }
